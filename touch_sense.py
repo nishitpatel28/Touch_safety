07ead0a4789c88180eb9dcf87b1160b23a86e5c4
nkp281297 <nkp281297@gmail.com>
2017-12-16 18:24:06 +0530

    touch_sense.py

---
+++ touch_sense.py
@@ -0,0 +1,48 @@
+import RPi.GPIO as GPIO
+import time
+import os
+import sys
+TouchPin = 27
+button = 22
+timeout = time.time() + 5
+def setup():
+        GPIO.setmode(GPIO.BCM)       # Numbers GPIOs by physical location
+        GPIO.setup(TouchPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(17,GPIO.OUT)
+        GPIO.setup(button,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+def loop():
+        print("s")
+        while True:                
+                inputval = GPIO.input(button)
+                if(time.time()>timeout):
+                        GPIO.output(17,GPIO.LOW)
+                        break
+                elif(inputval==True):
+                        GPIO.output(17,GPIO.LOW)
+                        print('No action taken')
+                        break                    
+                          
+                
+			
+
+def destroy():
+        GPIO.output(17, GPIO.LOW)     # led off
+if __name__ == "__main__":     # Program start from here
+        setup()
+        try:
+                while True:
+                        if(GPIO.input(TouchPin) == GPIO.HIGH):
+                                print 'T'
+                                GPIO.output(17,GPIO.HIGH)
+                                loop()
+                                print("sms sent")
+				                        os.system("python send_sms.py")
+				                        break
+				#os.system("python sms_sent.py")
+                        
+		
+		#os.system("python send_sms.py")
+                		
+		
+        except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+                destroy()
